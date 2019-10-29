7ef416a93da046da9d57c6998c4a19fc6499dc9e
root <root@Faithtower-Media.localdomain>
2019-10-29 10:33:13 +0000

    initial upload

---
+++ src/io/turntabl/code/review/Main.java
@@ -0,0 +1,56 @@
+package io.turntabl.code.review;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> oldUsers = Arrays.asList(
+                "Alex",
+                "David",
+                "Yaa"
+        );
+
+        List<String> users = new ArrayList<>();
+        for(String oldUser : oldUsers){
+            users.add(oldUser);
+        }
+
+        addUser("Shadrack", users);
+
+        System.out.println("\n");
+        removeLastUser(users);
+
+    }
+
+    public static String addUser(String newUser, List<String> users){
+        users.add(newUser);
+
+        if(users.size() <= 0){
+            System.out.println("There are no Users in the Database");
+        }else {
+            for(String user : users){
+                System.out.println(user);
+            }
+        }
+        return null;
+    }
+
+    public static String removeLastUser(List<String> users){
+        users.remove(users.size() - 1);
+
+        if(users.size() <= 0){
+            System.out.println("There are no Users in the Database");
+        }else {
+            for(String user : users){
+                System.out.println(user);
+            }
+        }
+        return null;
+    }
+
+}
+
+
